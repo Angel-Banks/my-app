805f446c91184e7a42ef9f57cd147a49a0403eb7
Angel-Banks <angelonuchuks@gmail.com>
2022-08-14 22:41:10 +0100

    editedComponent

---
+++ src/Component/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer (){
+    var year = new Date().getFullYear();
+
+    return <p>Copyright ⓒ {year}</p>
+}
+
+  
+
+   
+   
+
+
+
+
+export default Footer;
